543667e43be96b74d16ada3aa842abd744dcac2f
Andras Varga <vargan89@gmail.com>
2024-11-06 21:11:30 +0800

    keeper app part2 - rendering notes

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+// function createNote(noteItem) {
+//   return (
+//     <Note key={noteItem.id} title={noteItem.title} content={noteItem.content} />
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.id}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
